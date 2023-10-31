0aa5a1eea3d62288cdfcb2dacf69ebc0d121f18d
Shaz Younas <132129127+shazb9@users.noreply.github.com>
2023-10-31 22:06:03 +0500

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://shazbaloch:<mern>@bookstore-mern.3uh94sv.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
